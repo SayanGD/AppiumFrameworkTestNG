26ff2a4f556c459717133cd5caff3e95a47b86c3
Sayan Ghosh Dastidar <sayangd.41@gmail.com>
2025-03-15 13:05:01 +0530

    Test commit

--- src/main/java/com/ecommerceApp/pageObjects/ProductsPage.java
+++ src/main/java/com/ecommerceApp/pageObjects/ProductsPage.java
@@ -4,7 +4,6 @@ import java.util.List;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
